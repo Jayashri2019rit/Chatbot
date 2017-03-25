6056597dd68f5fed02c6cea4334a352d11382a05
Istvan Marki <istv0050@edu.easj.dk>
2017-03-25 11:45:30 +0100

    waterfall demo (console connector)

--- hello.js
+++ hello.js
@@ -7,7 +7,21 @@ var connector = new builder.ConsoleConnector().listen();
 //create the bot
 var bot = new builder.UniversalBot(connector);
 
-//add in the dialog
-bot.dialog('/', function(session){
-    session.send('Hello, bot!')
-});
+//add in the dialog and the logic
+// bot.dialog('/', function(session){
+//     //session.send('Hello, bot!')
+//     var userMessage = session.message.text;
+//     session.send('you said: ' + userMessage);
+// });
+
+//waterfall dialog, an array of functions:
+
+bot.dialog('/', [
+    function(session) {
+        builder.Prompts.text(session, 'Please enter your name');
+    },
+    function(session, result){
+        session.send('Hello, ' + result.response);
+    }
+
+]);
