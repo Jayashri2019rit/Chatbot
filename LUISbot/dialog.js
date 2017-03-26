80fbbae970a26e51ad25ed8c1b99f97c1342d4f8
Istvan Marki <istv0050@edu.easj.dk>
2017-03-26 13:22:54 +0200

    hosting bot in azure

---
+++ LUISbot/dialog.js
@@ -0,0 +1,5 @@
+var builder = require('botbuilder');
+
+var https = require('https');
+var querystring = require('querystring');
+var prompts = require('./prompts');
---
+++ LUISbot/prompts.js
@@ -0,0 +1,3 @@
+module.exports = {
+    userWelcomeMessage: 'Hi there! My name is botY, I can look up user information in GitHub!'
+}
